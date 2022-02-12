9bba1c790afae1ec6dc152b5bfb9d8f215e44369
kollerlukas <lukaskoller6@gmail.com>
2022-02-12 16:35:36 +0100

    upgraded to Vue3

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 /* global Vue, myApps, icons */
 
-/* init VueJs app */
-new Vue({
-    el: "#app",
-    components: {
-        /* register local svg-icon component */
-        "svg-icon": {
-            props: ["svg"],
-            template: '<span :class="svg.class" :title="svg.title"><svg :width="svg.width" :height="svg.height" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path v-for="path in svg.paths" :d="path.path" :fill="path.fill" :fill-opacity="path.opacity" /></svg></span>'
-        }
-    },
-    data: {
-        /* set apps data from myapps.js */
-        /* jslint es6 */
-        myApps: myApps,
-        /* set icons data from icons.js */
-        /* jslint es6 */
-        icons: icons,
-    },
-});
+Vue.createApp({
+  components: {
+    /* register local svg-icon component */
+    "svg-icon": {
+      props: ["svg"],
+      template: '<span :class="svg.class" :title="svg.title"><svg :width="svg.width" :height="svg.height" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path v-for="path in svg.paths" :d="path.path" :fill="path.fill" :fill-opacity="path.opacity" /></svg></span>'
+    }
+  },
+  data() {
+    return {
+      /* set apps data from myapps.js */
+      /* jslint es6 */
+      myApps: myApps,
+      /* set icons data from icons.js */
+      /* jslint es6 */
+      icons: icons,
+    }
+  },
+}).mount('#app')
